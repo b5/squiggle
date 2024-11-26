8c2b635bb05079c4fd59999648eb8e77054e9701
b5 <sparkle_pony_2000@qri.io>
2024-11-26 16:21:14 -0500

    remove explorer

--- node/src/main.rs
+++ node/src/main.rs
@@ -1,4 +1,6 @@
 use anyhow::{Context, Result};
+use squiggle_node::space::programs::Manifest;
+use std::collections::HashMap;
 
 use squiggle_node::node::Node;
 // use squiggle_node::space::programs::Manifest;
@@ -10,7 +12,7 @@ async fn main() -> Result<()> {
 
     let accounts = node.accounts().list(0, -1).await?;
     let user = accounts.first().expect("user to exist");
-    // let author = user.author.clone().expect("author to exist");
+    let author = user.author.clone().expect("author to exist");
 
     let space = node
         .spaces()
@@ -19,34 +21,34 @@ async fn main() -> Result<()> {
         .await?;
 
     // importing a program & running:
-    // let file = tokio::fs::read("../programs/github_repo_stargazers/dist/program.json").await?;
-    // let manifest: Manifest = serde_json::from_slice(file.as_slice())?;
+    let file = tokio::fs::read("../programs/github_repo_stargazers/dist/program.json").await?;
+    let manifest: Manifest = serde_json::from_slice(file.as_slice())?;
 
-    // let program = match space.programs().get_by_name(manifest.name).await {
-    //     Ok(program) => program,
-    //     Err(_) => {
-    //         space
-    //             .programs()
-    //             .create(author.clone(), "../programs/github_repo_stargazers/dist")
-    //             .await?
-    //     }
-    // };
+    let program = match space.programs().get_by_name(manifest.name).await {
+        Ok(program) => program,
+        Err(_) => {
+            space
+                .programs()
+                .create(author.clone(), "../programs/github_repo_stargazers/dist")
+                .await?
+        }
+    };
 
-    // let mut env = HashMap::new();
-    // env.insert("org".to_string(), "n0-computer".to_string());
-    // env.insert("repo".to_string(), "awesome-iroh".to_string());
-    // env.insert("github_token".to_string(), "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".to_string());
+    let mut env = HashMap::new();
+    env.insert("org".to_string(), "n0-computer".to_string());
+    env.insert("repo".to_string(), "awesome-iroh".to_string());
+    env.insert("github_token".to_string(), "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".to_string());
 
-    // space
-    //     .secrets()
-    //     .set_for_program_id(author.clone(), program.id, env)
-    //     .await?;
+    space
+        .secrets()
+        .set_for_program_id(author.clone(), program.id, env)
+        .await?;
 
-    // let res = node
-    //     .vm()
-    //     .run_program(&space, author, program.id, HashMap::new())
-    //     .await?;
-    // println!("Flow output: {:?}", res);
+    let res = node
+        .vm()
+        .run_program(&space, author, program.id, HashMap::new())
+        .await?;
+    println!("Flow output: {:?}", res);
 
     let ticket = space.share().await?;
     println!("Space ticket: {}", ticket);
